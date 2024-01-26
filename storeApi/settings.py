b08d8d98fdfdad71493ffedeb84d5b56e79169ee
Idrissa <idrisabkar2018@gmail.com>
2024-01-26 23:32:40 +0300

    quelques retouches

--- storeApi/settings.py
+++ storeApi/settings.py
@@ -192,5 +192,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:9000",
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "http://localhost:5174"
 ]
