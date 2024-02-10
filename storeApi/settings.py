5e5737a129855cb5d9041efd5d2521831e28b0b7
Idrissa <64365948+idrissruso@users.noreply.github.com>
2024-02-10 19:32:25 +0300

    Update settings.py

--- storeApi/settings.py
+++ storeApi/settings.py
@@ -207,6 +207,4 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
-CORS_ALLOWED_ORIGINS = [
-    "*"
-]
+CORS_ALLOW_ALL_ORIGINS = True
