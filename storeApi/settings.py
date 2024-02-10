9c80280a498289dbbcd0b0d526bd68b484c9f6f5
Idrissa <idrisabkar2018@gmail.com>
2024-02-10 19:28:41 +0300

    quelques retouches

--- storeApi/settings.py
+++ storeApi/settings.py
@@ -208,10 +208,5 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:5174",
     "*"
 ]
